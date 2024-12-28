cd04d48858e0ef228eb6a178ceb1860806d59da4
netcanis <netcanis@gmail.com>
2024-12-29 00:59:35 +0900

    Initial commit: HiOcrSample app using feat_ocr for credit card scanning and OCR functionality.

---
+++ HiOcrSample/HiOcrSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiOcrSampleApp.swift
+//  HiOcrSample
+//
+//  Created by netcanis on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct HiOcrSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
